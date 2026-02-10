f0ff6aab554d60ff619a06b31f819909317ba16a
lewkus-dev <lukas.hjartat@gmail.com>
2026-02-10 16:32:10 +0100

    added the class, repository and the sql file with CREATE TABLE

--- sql/Book.sql
+++ sql/Book.sql
@@ -1,8 +1,8 @@
 CREATE TABLE book
 (
-    isbn   LONG UNIQUE NOT NULL,
-    title  VARCHAR(20) NOT NULL,
-    author VARCHAR(20) NOT NULL,
-    SAB    VARCHAR(5),
-    DDC    VARCHAR(5)
+    id     LONG AUTO_INCREMENT NOT NULL UNIQUE PRIMARY KEY,
+    isbn   LONG UNIQUE         NOT NULL,
+    title  VARCHAR(20)         NOT NULL,
+    author VARCHAR(20)         NOT NULL,
+    genre  VARCHAR(20)         NOT NULL
 );
