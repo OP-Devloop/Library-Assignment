224a6e3cfe45ff7065566c25a49f20eeaeda482e
OP-Devloop <oscar.pettersson@iths.se>
2026-02-10 10:44:32 +0100

    initial commit

---
+++ src/test/java/se/iths/oscarp/libraryassignment/LibraryAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.oscarp.libraryassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
