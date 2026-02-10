f4056ce4db08c95f58b1a19e57c6d84280c58b97
OP-Devloop <oscar.pettersson@iths.se>
2026-02-10 12:48:36 +0100

    made audiobook entity and repo

---
+++ src/main/java/se/iths/oscarp/libraryassignment/repository/AudiobookRepository.java
@@ -0,0 +1,9 @@
+package se.iths.oscarp.libraryassignment.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import se.iths.oscarp.libraryassignment.model.Audiobook;
+
+@Repository
+public interface AudiobookRepository extends JpaRepository<Audiobook, String> {
+}
