b7bfc22092747595f4d5dbfaf476673e8cd8f26a
fabian-berencreutz <fabianberencreutz@gmail.com>
2026-02-12 10:01:44 +0100

    Added movieEntity, repo, validator, sql, excpetions.

---
+++ src/main/java/se/iths/oscarp/libraryassignment/exception/MovieValidationException.java
@@ -0,0 +1,7 @@
+package se.iths.oscarp.libraryassignment.exception;
+
+public class MovieValidationException extends RuntimeException {
+    public MovieValidationException(String message) {
+        super(message);
+    }
+}
