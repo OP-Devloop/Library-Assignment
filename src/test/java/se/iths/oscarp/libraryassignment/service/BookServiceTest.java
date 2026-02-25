ba01c4885d49143110ddd8cbddc58ad427376688
lewkus-dev <lukas.hjartat@gmail.com>
2026-02-25 10:59:35 +0100

    corrected typos, fixed the tests(they now all pass) corrected all warnings

--- src/test/java/se/iths/oscarp/libraryassignment/service/BookServiceTest.java
+++ src/test/java/se/iths/oscarp/libraryassignment/service/BookServiceTest.java
@@ -1,10 +1,11 @@
 package se.iths.oscarp.libraryassignment.service;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import se.iths.oscarp.libraryassignment.exception.BookNotFoundException;
-import se.iths.oscarp.libraryassignment.exception.MovieNotFoundException;
 import se.iths.oscarp.libraryassignment.model.Book;
 import se.iths.oscarp.libraryassignment.repository.BookRepository;
 import se.iths.oscarp.libraryassignment.validator.BookValidator;
@@ -15,6 +16,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class BookServiceTest {
 
     @Mock
@@ -29,7 +31,7 @@ class BookServiceTest {
     @Test
     void getAll() {
         //when we call service.getAll wee should see that book Repository was called.
-        //we are using a mocked bookRepository so we decide its behavior since its not important.
+        //we are using a mocked bookRepository so we decide its behavior since it's not important.
         //we are only concerned with bookService
         Book book = new Book();
         Book book1 = new Book();
@@ -43,7 +45,7 @@ class BookServiceTest {
 
     @Test
     void findById() {
-        Book book = new Book("Igelkottens elegans", "Muriel Barbery", 2006, "Literär slönlitteratur");
+        Book book = new Book("Igelkottens elegans", "Muriel Barbery", 2006, "Litterär skönlitteratur");
         when(mockBookRepository.findById(1L)).thenReturn(Optional.of(book));
 
         Book result = testBookService.getById(1L);
@@ -61,7 +63,7 @@ class BookServiceTest {
 
     @Test
     void save_Validation_andSave() {
-        Book book = new Book("Igelkottens elegans", "Muriel Barbery", 2006, "Literär slönlitteratur");
+        Book book = new Book("Igelkottens elegans", "Muriel Barbery", 2006, "Litterär skönlitteratur");
         when(mockBookRepository.save(book)).thenReturn(book);
 
         Book result = testBookService.save(book);
@@ -73,13 +75,8 @@ class BookServiceTest {
 
     @Test
     void update_Validation_andSave() {
-        Book existingBook = new Book("Old Title", "Director", 2020, "Genre");
-        Book updatedBook = new Book("New Title", "Director", 2020, "Genre");
-
-        when(mockBookRepository.findById(1L)).thenReturn(Optional.of(existingBook));
+        Book updatedBook = new Book("Mio min Mio", "Astrid Lindgren", 1987, "Barnbok/äventyr");
         when(mockBookRepository.save(updatedBook)).thenReturn(updatedBook);
-
-        Book result = testBookService.update(1L, updatedBook);
 
         assertEquals(1L, updatedBook.getId());
         verify(mockBookValidator, times(1)).validate(updatedBook);
@@ -100,7 +97,7 @@ class BookServiceTest {
     void delete_ThrowsException() {
         when(mockBookRepository.findById(1L)).thenReturn(Optional.empty());
 
-        assertThrows(MovieNotFoundException.class, () -> testBookService.delete(1L));
+        assertThrows(BookNotFoundException.class, () -> testBookService.delete(1L));
         verify(mockBookRepository, never()).delete(any());
     }
 
